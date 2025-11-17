d5cd13e60044bc1cbdc6c3031d256c0cf6cf90d1
Fowzi Hindi <fowzi.hindi@sabanciuniv.edu>
2025-11-17 23:16:51 +0300

    fixed flashcards routes

--- lib/router/flashcards_router.dart
+++ lib/router/flashcards_router.dart
@@ -1,9 +1,2 @@
-import 'package:go_router/go_router.dart';
-import '../features/flashcards/flashcards_screen.dart';
 
-final flashcardsRoutes = [
-  GoRoute(path: '/flashcards', builder: (_, __) => const FlashcardsScreen()),
-];
-
-/// Routes for Flashcards feature.
 /// - List/study routes later: '/flashcards', '/flashcards/:deckId/study'.
