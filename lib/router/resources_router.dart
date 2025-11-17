cb80f4d978bf8c47499c8bec94c7b195e1549703
SalmaTubail <salma.tubail@sabanciuniv.edu>
2025-11-17 19:43:21 +0300

    Add ResourcesRouter for resource navigation
    Implement a router for managing resources navigation.

---
+++ lib/router/resources_router.dart
@@ -0,0 +1,41 @@
+
+
+import 'package:flutter/material.dart';
+import 'resources_list_screen.dart';
+import 'add_resource_screen.dart';
+import 'resource_details_screen.dart';
+
+class ResourcesRouter {
+  static Map<String, WidgetBuilder> routes = {
+
+    '/resources': (context) {
+      final args =
+          ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+      return ResourcesListScreen(
+        courseId: args['courseId'],
+        courseName: args['courseName'],
+      );
+    },
+
+    '/addResource': (context) {
+      final args =
+          ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+      return AddResourceScreen(
+        courseId: args['courseId'],
+        courseName: args['courseName'],
+      );
+    },
+    '/resourceDetails': (context) {
+      final args =
+          ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+      return ResourceDetailsScreen(
+        resource: args['resource'],     // pass Resource object
+        courseId: args['courseId'],
+        courseName: args['courseName'],
+      );
+    },
+  };
+}
